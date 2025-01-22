fb362bc5478379efe5c1536d4ea0cae587070f90
Tony Kipkemboi <iamtonykipkemboi@gmail.com>
2025-01-22 06:05:26 -0500

    fix: restore original layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -116,8 +116,8 @@ export default function RootLayout({
           `}
         </Script>
       </head>
-      <body className="antialiased">
-        <main className="max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
           <Footer />
